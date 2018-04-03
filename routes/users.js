43ba8c70910cbf03331692ac441b424a650aab39
Joshua Hutchinson <jthutch94@gmail.com>
2018-04-02 20:57:46 -0400

    validations added

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,83 @@
 'use strict';
-
 const express = require('express');
+
+const User = require('../models/user');
+
 const router = express.Router();
 
-const  User = require('../models/user');
-
+/* ========== POST/CREATE AN ITEM ========== */
 router.post('/users', (req, res, next) => {
+
   const requiredFields = ['username', 'password'];
   const missingField = requiredFields.find(field => !(field in req.body));
 
   if (missingField) {
-    return res.status(422).json({
-      code: 422,
-      reason: 'ValidationError',
-      message: 'Missing Field',
-      location: missingField
-    });
+    const err = new Error(`Missing '${missingField}' in request body`);
+    err.status = 422;
+    return next(err);
   }
 
-  const stringFields = ['fullName', 'username', 'password'];
-  const nonStringField = stringFields.find( field => field in req.body && typeof req.body[field] !== 'string');
+  const stringFields = ['username', 'password', 'fullname'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
 
-  if (nonStringField){
-    return res.status(422).json({
-      code: 422,
-      reason: 'ValidationError',
-      message: 'Incorrect field type: expected string',
-      location: nonStringField
-    });
+  if (nonStringField) {
+    const err = new Error(`Field: '${nonStringField}' must be type String`);
+    err.status = 422;
+    return next(err);
   }
 
-  let { username, password, fullName = '' } = req.body;
-  fullName = fullName.trim();
+  
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with whitespace`);
+    err.status = 422;
+    return next(err);
+  }
+
+
+  const sizedFields = {
+    username: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  if (tooSmallField) {
+    const min = sizedFields[tooSmallField].min;
+    const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField) {
+    const max = sizedFields[tooLargeField].max;
+    const err = new Error(`Field: '${tooSmallField}' must be at most ${max} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  let { username, password, fullname = '' } = req.body;
+  fullname = fullname.trim();
 
   return User.hashPassword(password)
     .then(digest => {
       const newUser = {
         username,
         password: digest,
-        fullName
+        fullname
       };
       return User.create(newUser);
     })
